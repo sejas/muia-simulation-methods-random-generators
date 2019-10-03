491728c66d38a2edd5c16bcc4fa761c76b3f1ec9
Sejas <antonio@sejas.es>
2019-10-03 10:44:32 +0200

    feat: 🚀 Program in C and contrast results for IMSL

---
+++ TestU01-1.2.3/examples/muia.c
@@ -0,0 +1,14 @@
+#include "ulcg.h"
+#include "unif01.h"
+#include "bbattery.h"
+
+int main (void)
+{
+   unif01_Gen *gen;
+   // 2147483647 is 2^31 - 1 
+                        // m        , a,    b, x0
+   gen = ulcg_CreateLCG (2147483647, 16807, 0, 1);
+   bbattery_SmallCrush (gen);
+   ulcg_DeleteGen (gen);
+   return 0;
+}
