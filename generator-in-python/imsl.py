34e42b8b3067382c980d9f74e0b77a69a4ba1757
Sejas <antonio@sejas.es>
2019-12-20 00:30:14 +0100

    feat: add Schrage algorithm. fix #3

--- generator-in-python/imsl.py
+++ generator-in-python/imsl.py
@@ -9,16 +9,19 @@ Authors: Antonio Sejas, Danielle Pellegrino, Sergio Cavero
 class IMSL_BASIC:
     def __init__(self, x0=1, a=16807, m=2**31-1, b=0):
         self.xi = x0
+        self.xi_interval_0_1 = x0/m
         self.a = a
         self.m = m
         self.b = b
 
     def gen(self):
         self.xi = (self.a*self.xi+self.b) % self.m
+        self.xi_interval_0_1 = self.xi / self.m
         return self.xi
 
     def gen_interval_0_1(self):
-        return self.gen()/self.m
+        self.gen()
+        return self.xi_interval_0_1
 
 
 class IMSL:
@@ -26,13 +29,25 @@ class IMSL:
 
     def __init__(self, x0=1, a=16807, m=2**31-1, b=0):
         self.xi = x0
+        self.xi_interval_0_1 = x0/m
         self.a = a
         self.m = m
         self.b = b
+        # Schrage extra variables
+        self.q = m/a
+        self.r = m % a
 
     def gen(self):
-        self.xi = (self.a*self.xi+self.b) % self.m
+        hi = int(self.xi / self.q)
+        lo = self.xi % self.q
+        test = self.a * lo - self.r*hi
+        if test > 0:
+            self.xi = test
+        else:
+            self.xi = test + self.m
+        self.xi_interval_0_1 = self.xi / self.m
         return self.xi
 
     def gen_interval_0_1(self):
-        return self.gen()/self.m
+        self.gen()
+        return self.xi_interval_0_1
